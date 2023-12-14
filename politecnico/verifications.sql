eb7f1b9f25b1263d32d643095a2499236fa8dc24
Kadu Pantoja <dsc.pantoja@gmail.com>
2023-12-13 21:01:42 -0300

    Arquivos de Verificação da Atividade de Inserção na base Politécnico.

--- politecnico/verifications.sql
+++ politecnico/verifications.sql
@@ -1,20 +1,20 @@
-select
+SELECT
     p.nome as 'Instituto Politécnico',
     c.nome as 'Licenciatura',
     d.nome as 'Disciplina',
     gd.periodo,
     d.ects
-from disciplina d
-    join grade_disciplina gd on gd.idDisciplina = d.idDisciplina
-    join grade g on g.idGrade = gd.idGrade
-    join curso c on c.idCurso = g.idCurso
-    join politecnico p on p.idPolitecnico = c.idPolitecnico
-where c.idCurso = 15
-order by
+FROM disciplina d
+    INNER JOIN grade_disciplina gd ON (gd.idDisciplina = d.idDisciplina)
+    INNER JOIN grade g ON (g.idGrade = gd.idGrade)
+    INNER JOIN curso c ON (c.idCurso = g.idCurso)
+    INNER JOIN politecnico p ON (p.idPolitecnico = c.idPolitecnico)
+WHERE c.idCurso = 15
+ORDER BY
     gd.periodo asc,
     d.nome asc;
 
-select
+SELECT
     distinct concat(a.nome, ' ', a.apelido) as 'Aluno',
     p.nome as 'Instituto',
     c.idCurso,
@@ -22,12 +22,12 @@ select
     d.nome as 'Disciplina',
     h.nota as 'Nota Final',
     h.status
-from aluno a
-    join aluno_grade ag on a.idAluno = ag.idAluno
-    join historico h on h.matricula = ag.matricula
-    join grade_disciplina gd on gd.idDisciplina = h.idDisciplina
-    join grade g on g.idGrade = gd.idGrade
-    join disciplina d on d.idDisciplina = gd.idDisciplina
-    join curso c on c.idCurso = g.idCurso
-    join politecnico p on p.idPolitecnico = c.idPolitecnico
-where a.idAluno IN (13, 14);
+FROM aluno a
+    INNER JOIN aluno_grade ag ON (a.idAluno = ag.idAluno)
+    INNER JOIN historico h ON (h.matricula = ag.matricula)
+    INNER JOIN grade_disciplina gd ON (gd.idDisciplina = h.idDisciplina)
+    INNER JOIN grade g ON (g.idGrade = gd.idGrade)
+    INNER JOIN disciplina d ON (d.idDisciplina = gd.idDisciplina)
+    INNER JOIN curso c ON (c.idCurso = g.idCurso)
+    INNER JOIN politecnico p ON (p.idPolitecnico = c.idPolitecnico)
+WHERE a.idAluno IN (13, 14);
